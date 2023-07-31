ed806e76b11fee80ee96b4083445ccfef0185022
ilysharusher <illyaetoilysha@gmail.com>
2023-07-31 18:23:39 +0300

    Computed Data and Conditional Rendering Displaying Flash Messages

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,22 @@
 <script setup>
+import { computed } from 'vue';
+import { usePage } from '@inertiajs/vue3';
 
+const flashSuccess = computed(
+    () => usePage().props.flash.success,
+);
 </script>
 
 <template>
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
     <slot />
 </template>
 
 <style scoped>
-
+.success {
+    background-color: green;
+    color: white;
+}
 </style>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('listing', ListingController::class);
+Route::resource('listing', App\Http\Controllers\ListingController::class);
 
 Route::get('/', [App\Http\Controllers\IndexController::class, 'index']);
 Route::get('/show', [App\Http\Controllers\IndexController::class, 'show']);
