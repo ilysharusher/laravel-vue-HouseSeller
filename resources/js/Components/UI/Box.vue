2829d179f9fd0b0a2f18dbc252967dc42f2f4daf
ilysharusher <illyaetoilysha@gmail.com>
2023-08-03 18:07:28 +0300

    Custom UI Components and Grid Responsive Design

---
+++ resources/js/Components/UI/Box.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="border border-gray-200 dark:border-gray-800 rounded-md shadow-sm p-4 dark:text-gray-300">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
+import Box from '@/Components/UI/Box.vue';
 
 defineProps({
     listings: {
@@ -10,20 +11,22 @@ defineProps({
 </script>
 
 <template>
-    <div
-        v-for="listing in listings"
-        :key="listing.id"
-    >
-        <div>
-            <Link :href="route('listing.show', listing.id)">{{ listing.id }}</Link>
-        </div>
-        <div>
-            <Link :href="route('listing.edit', listing.id)">Edit</Link>
-        </div>
-        <div>
-            <Link :href="route('listing.destroy', listing.id)" method="delete" as="button">Delete</Link>
-        </div>
-        <br />
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Box
+            v-for="listing in listings"
+            :key="listing.id"
+        >
+            <div>
+                <Link :href="route('listing.show', listing.id)">{{ listing.id }}</Link>
+            </div>
+            <div>
+                <Link :href="route('listing.edit', listing.id)">Edit</Link>
+            </div>
+            <div>
+                <Link :href="route('listing.destroy', listing.id)" method="delete" as="button">Delete</Link>
+            </div>
+            <br />
+        </Box>
     </div>
 </template>
 
