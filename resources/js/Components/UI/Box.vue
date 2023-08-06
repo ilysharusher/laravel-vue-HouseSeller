d687dc1fcdfa89586939fe15702c47c5a17ee9a3
ilysharusher <illyaetoilysha@gmail.com>
2023-08-06 19:18:06 +0300

    Listing Page Getting Even Better

--- resources/js/Components/UI/Box.vue
+++ resources/js/Components/UI/Box.vue
@@ -4,6 +4,9 @@
 
 <template>
     <div class="border border-gray-200 dark:border-gray-800 rounded-md shadow-sm p-4 dark:text-gray-300">
+        <div class="font-medium text-gray-500">
+            <slot name="title" />
+        </div>
         <slot />
     </div>
 </template>
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,5 +1,8 @@
 <script setup>
 import ListingAdress from '@/Components/Listing/ListingAdress.vue';
+import ListingPrice from '@/Components/Listing/ListingPrice.vue';
+import ListingSpace from '@/Components/Listing/ListingSpace.vue';
+import Box from '@/Components/UI/Box.vue';
 
 defineProps({
     listing: {
@@ -10,7 +13,29 @@ defineProps({
 </script>
 
 <template>
-    <ListingAdress :listing="listing" />
+    <div class="flex flex-col-reverse lg:grid lg:grid-cols-12 gap-4">
+        <Box class="lg:col-span-7 flex items-center w-full">
+            <div class="w-full text-center font-medium text-gray-500">
+                No image yet :(
+            </div>
+        </Box>
+        <div class="lg:col-span-5 flex flex-col gap-4">
+            <Box>
+                <template #title>
+                    About this listing
+                </template>
+                <ListingPrice :price="listing.price" class="text-2xl font-bold" />
+                <ListingSpace :listing="listing" class="text-lg" />
+                <ListingAdress :listing="listing" class="text-gray-500" />
+            </Box>
+            <Box>
+                <template #title>
+                    Buy this home
+                </template>
+                Make an offer
+            </Box>
+        </div>
+    </div>
 </template>
 
 <style scoped>
