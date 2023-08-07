63ab0ba345a23aa5d19669ab12681317269c7195
ilysharusher <illyaetoilysha@gmail.com>
2023-08-07 19:31:36 +0300

    Styling Error Messages

---
+++ resources/js/Components/Errors/InputError.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    errorMessage: String,
+});
+</script>
+
+<template>
+    <div class="input-error">
+        {{ errorMessage }}
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Listing/Edit.vue
+++ resources/js/Pages/Listing/Edit.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useForm } from '@inertiajs/vue3';
+import InputError from '@/Components/Errors/InputError.vue';
 
 const props = defineProps({
     listing: Object,
@@ -19,65 +20,49 @@ const submit = () => form.patch(route('listing.update', props.listing.id));
             <div class="col-span-2">
                 <label class="label">Beds</label>
                 <input v-model.number="form.beds" type="text" class="input" />
-                <div v-if="form.errors.beds" class="error">
-                    {{ form.errors.beds }}
-                </div>
+                <InputError v-if="form.errors.beds" :error-message="form.errors.beds" />
             </div>
 
             <div class="col-span-2">
                 <label class="label">Baths</label>
                 <input v-model.number="form.baths" type="text" class="input" />
-                <div v-if="form.errors.baths" class="error">
-                    {{ form.errors.baths }}
-                </div>
+                <InputError v-if="form.errors.baths" :error-message="form.errors.baths" />
             </div>
 
             <div class="col-span-2">
                 <label class="label">Area</label>
                 <input v-model.number="form.area" type="text" class="input" />
-                <div v-if="form.errors.area" class="error">
-                    {{ form.errors.area }}
-                </div>
+                <InputError v-if="form.errors.area" :error-message="form.errors.area" />
             </div>
 
             <div class="col-span-4">
                 <label class="label">City</label>
                 <input v-model="form.city" type="text" class="input" />
-                <div v-if="form.errors.city" class="error">
-                    {{ form.errors.city }}
-                </div>
+                <InputError v-if="form.errors.city" :error-message="form.errors.city" />
             </div>
 
             <div class="col-span-2">
                 <label class="label">Post Code</label>
                 <input v-model="form.code" type="text" class="input" />
-                <div v-if="form.errors.code" class="error">
-                    {{ form.errors.code }}
-                </div>
+                <InputError v-if="form.errors.code" :error-message="form.errors.code" />
             </div>
 
             <div class="col-span-4">
                 <label class="label">Street</label>
                 <input v-model="form.street" type="text" class="input" />
-                <div v-if="form.errors.street" class="error">
-                    {{ form.errors.street }}
-                </div>
+                <InputError v-if="form.errors.street" :error-message="form.errors.street" />
             </div>
 
             <div class="col-span-2">
                 <label class="label">Street Nr</label>
                 <input v-model="form.street_number" type="text" class="input" />
-                <div v-if="form.errors.street_number" class="error">
-                    {{ form.errors.street_number }}
-                </div>
+                <InputError v-if="form.errors.street_number" :error-message="form.errors.street_number" />
             </div>
 
             <div class="col-span-6">
                 <label class="label">Price</label>
                 <input v-model.number="form.price" type="text" class="input" />
-                <div v-if="form.errors.price" class="error">
-                    {{ form.errors.price }}
-                </div>
+                <InputError v-if="form.errors.price" :error-message="form.errors.price" />
             </div>
 
             <div class="col-span-6">
