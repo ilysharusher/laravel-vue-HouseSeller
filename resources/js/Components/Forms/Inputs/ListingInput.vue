112f8b9cff299fed35cb7dd925c60c184a1fdb16
ilysharusher <illyaetoilysha@gmail.com>
2023-08-08 09:43:55 +0300

    Fix errors messages

--- resources/js/Components/Forms/Inputs/ListingInput.vue
+++ resources/js/Components/Forms/Inputs/ListingInput.vue
@@ -12,7 +12,7 @@ defineProps({
     <div>
         <label class="label">{{ label }}</label>
         <input v-model="form[content]" type="text" class="input" />
-        <InputError v-if="form.errors.content" :error-message="form.errors.content" />
+        <InputError v-if="form.errors[content]" :error-message="form.errors[content]" />
     </div>
 </template>
 
