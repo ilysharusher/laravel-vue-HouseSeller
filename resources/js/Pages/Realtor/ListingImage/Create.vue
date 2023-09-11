b0a02c36e433fedd6f28291222cc95c6842aee0c
Ilya <illyaetoilysha@gmail.com>
2023-09-11 17:29:34 +0300

    Styling the Upload Input

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Box from '@/Components/UI/Box.vue';
 import {useForm} from '@inertiajs/vue3';
+import {computed} from 'vue';
 
 const props = defineProps({
     listing: Object,
@@ -9,6 +10,10 @@ const props = defineProps({
 const form = useForm({
     images: [],
 });
+
+const canUpload = computed(
+    () => form.images.length,
+);
 
 const addImage = (event) => {
     for (const image of event.target.files) {
@@ -34,9 +39,30 @@ const reset = () => form.reset('images');
         <form
             @submit.prevent="submit"
         >
-            <input type="file" multiple @input="addImage" />
-            <button type="submit" class="btn-primary" @click="submit">Send</button>
-            <button type="reset" class="btn-secondary" @click="reset">Reset</button>
+            <section class="flex items-center gap-2 my-4">
+                <input
+                    class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
+                    type="file" multiple @input="addImage"
+                />
+                <button
+                    :class="{
+                        'opacity-25 pointer-events-none': !canUpload,
+                    }"
+                    type="submit"
+                    class="btn-primary"
+                >
+                    Upload
+                </button>
+                <button
+                    :class="{
+                        'opacity-25 pointer-events-none': !canUpload,
+                    }"
+                    type="reset" class="btn-secondary"
+                    @click="reset"
+                >
+                    Reset
+                </button>
+            </section>
         </form>
     </Box>
 </template>
