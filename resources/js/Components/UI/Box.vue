c89ce459ec4d406c1fd1ff536ab9f3e7ffc4d019
Ilya <illyaetoilysha@gmail.com>
2023-09-12 12:50:26 +0300

    Restructure Upload zone

--- resources/js/Components/UI/Box.vue
+++ resources/js/Components/UI/Box.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="border border-gray-200 dark:border-gray-800 rounded-2xl shadow-sm p-4 dark:text-gray-300">
-        <div class="font-medium text-gray-500 mb-1">
+        <div class="font-medium text-gray-500">
             <slot name="title" />
         </div>
         <slot />
--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -24,10 +24,9 @@ const addImage = (event) => {
 const submit = () => {
     form.post(
         route('realtor.listing.image.store', props.listing.id),
-        {
-            onSuccess: () => form.reset('images'),
-        },
     );
+
+    reset();
 };
 
 const reset = () => form.reset('images');
@@ -39,11 +38,20 @@ const reset = () => form.reset('images');
         <form
             @submit.prevent="submit"
         >
-            <section class="flex items-center gap-2 my-4">
-                <input
-                    class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
-                    type="file" multiple @input="addImage"
-                />
+            <div class="flex items-center justify-center w-full my-4">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                        </svg>
+                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF</p>
+                    </div>
+                    <input id="dropzone-file" type="file" class="hidden" @input="addImage" />
+                </label>
+            </div>
+
+            <div>
                 <button
                     :class="{
                         'opacity-25 pointer-events-none': !canUpload,
@@ -62,7 +70,7 @@ const reset = () => form.reset('images');
                 >
                     Reset
                 </button>
-            </section>
+            </div>
         </form>
     </Box>
 </template>
