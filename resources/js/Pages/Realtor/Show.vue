6c5a5fc3d517e83b045f569c80184a4568f74c0b
Ilya <illyaetoilysha@gmail.com>
2023-10-11 20:34:51 +0300

    Offer List Page

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -1,9 +1,46 @@
 <script setup>
+import Box from '@/Components/UI/Box.vue';
+import ListingSpace from '@/Components/Listing/ListingSpace.vue';
+import ListingPrice from '@/Components/Listing/ListingPrice.vue';
+import ListingAdress from '@/Components/Listing/ListingAdress.vue';
+import { Link } from '@inertiajs/vue3';
+import {computed} from 'vue';
 
+const props = defineProps({
+    listing: Object,
+});
+
+const hasOffers = computed(() => props.listing.offers.length);
 </script>
 
 <template>
-    hi
+    <div class="mb-4">
+        <Link
+            :href="route('realtor.listing.index')"
+        >
+            ← Go back to Listings
+        </Link>
+    </div>
+
+    <section class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
+        <Box v-if="!hasOffers" class="flex md:col-span-7 items-center">
+            <div class="w-full text-center font-medium text-gray-500">
+                No offers
+            </div>
+        </Box>
+
+        <div v-else class="md:col-span-7 items-center">
+            This is displayed when there are offers!
+        </div>
+
+        <Box class="md:col-span-5">
+            <template #title>Basic Info</template>
+            <ListingPrice :price="listing.price" class="text-2xl font-bold" />
+
+            <ListingSpace :listing="listing" class="text-lg" />
+            <ListingAdress :listing="listing" class="text-gray-500" />
+        </Box>
+    </section>
 </template>
 
 <style scoped>
