bd77bad41a39247cb4c70e334b78c4d467be9630
Ilya <illyaetoilysha@gmail.com>
2023-10-25 15:11:39 +0300

    Restyle a VerifyEmail page

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,14 +1,21 @@
 <script setup>
-import {Link} from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
+import EmptyPlace from '@/Components/UI/EmptyPlace.vue';
 </script>
 
 <template>
-    <div>
-        <h1>Verify Email</h1>
-    </div>
-    <div>
-        <Link :href="route('verification.send')" method="post" as="button" class="btn-secondary">Resend link</Link>
-    </div>
+    <EmptyPlace>
+        <div>
+            <p>Oops! It looks like you haven't verified your email yet :(</p>
+            <Link
+                :href="route('verification.send')"
+                method="post"
+                class="text-blue-500"
+            >
+                But you can receive a new verification link!
+            </Link>
+        </div>
+    </EmptyPlace>
 </template>
 
 <style scoped>
