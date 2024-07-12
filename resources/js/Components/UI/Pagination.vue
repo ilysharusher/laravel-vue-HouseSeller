6e4656f2f438abbd5f8452022524395d085a2ff2
Ilya <illyaetoilysha@gmail.com>
2024-07-12 14:51:02 +0000

    Pagination fix in the extremes

--- resources/js/Components/UI/Pagination.vue
+++ resources/js/Components/UI/Pagination.vue
@@ -11,10 +11,22 @@ const paginatedLinks = props.links.slice(1, -1);
 <template>
     <ul class="flex items-center -space-x-px h-10 text-base">
         <li>
-            <Link :href="links[0].url ?? 'null'" class="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-xl hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            <Link
+                :href="links[0].url ?? 'null'"
+                class="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-xl hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                :class="{
+                    'opacity-50 pointer-events-none': !links[0].url,
+                }"
+            >
                 <span class="sr-only">Previous</span>
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+                <svg
+                    class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 6 10"
+                >
+                    <path
+                        stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M5 1 1 5l4 4"
+                    />
                 </svg>
             </Link>
         </li>
@@ -34,10 +46,22 @@ const paginatedLinks = props.links.slice(1, -1);
             </Link>
         </li>
         <li>
-            <Link :href="links[links.length - 1].url ?? 'null'" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-xl hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            <Link
+                :href="links[links.length - 1].url ?? 'null'"
+                class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-xl hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                :class="{
+                    'opacity-50 pointer-events-none': !links[links.length - 1].url,
+                }"
+            >
                 <span class="sr-only">Next</span>
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                <svg
+                    class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 6 10"
+                >
+                    <path
+                        stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 9 4-4-4-4"
+                    />
                 </svg>
             </Link>
         </li>
